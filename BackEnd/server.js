6f7199c92cbed148b275ae61350d2b00896490ee
AnasTahir84 <anastahirhussain7@gmail.com>
2026-02-01 02:14:40 +0500

    phase one complete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,28 +1,32 @@
 import express from "express";
 import { configDotenv } from "dotenv";
-import connentDB from "./config/db.js";
-import cors from 'cors'
-import authRoutes from "./routes/authRoutes.js";
+import connectDB from "./config/db.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import clientsRouts from "./routes/clientsRoutes.js";
-import generateInvoiceRoutes from "./routes/generateInvoiceRoutes.js";
-import serviesRoutes from "./routes/serviesRoutes.js";
-import analyticsRoutes from "./routes/analyticsRoutes.js";
-configDotenv()
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
+import authRoutes from "./routes/authRoutes.js";
+import clientsRoutes from "./routes/clientsRoutes.js";
+import generateInvoiceRoutes from "./routes/generateInvoiceRoutes.js";
+import servicesRoutes from "./routes/serviesRoutes.js";
+import analyticsRoutes from "./routes/analyticsRoutes.js";
+
+configDotenv();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
-   origin: true, 
-   credentials: true 
-}))
-app.use('/auth',authRoutes)
-app.use(clientsRouts)
-app.use(generateInvoiceRoutes)
-app.use(serviesRoutes)
-app.use(analyticsRoutes)
-connentDB()
-app.listen(process.env.PORT,()=>{
-   console.log("SERVER IS RUNNING!");
-})
+  origin: true,
+  credentials: true
+}));
+
+connectDB();
+
+app.use("/auth", authRoutes);
+app.use(clientsRoutes);
+app.use(generateInvoiceRoutes);
+app.use(servicesRoutes);
+app.use(analyticsRoutes);
+
+export default app;
--- FrontEnd/src/utils/apiConstant.js
+++ FrontEnd/src/utils/apiConstant.js
@@ -1,1 +1,1 @@
-export const baseUrl = `http://localhost:8000`
+export const baseUrl = `https://invoice-gen-pro-sigma.vercel.app`
