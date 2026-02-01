d07ff7051e817206542a5192600dc709e9709325
AnasTahir84 <anastahirhussain7@gmail.com>
2026-02-02 00:32:53 +0500

    test

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,41 +1,67 @@
+// import express from "express";
+// import { configDotenv } from "dotenv";
+// import connectDB from "./config/db.js";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/authRoutes.js";
+// import clientsRoutes from "./routes/clientsRoutes.js";
+// import generateInvoiceRoutes from "./routes/generateInvoiceRoutes.js";
+// import servicesRoutes from "./routes/serviesRoutes.js";
+// import analyticsRoutes from "./routes/analyticsRoutes.js";
+
+// configDotenv();
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({
+//   origin: true,
+//   credentials: true,
+// }));
+
+// // 🔥 DB middleware (safe for serverless)
+// app.use(async (req, res, next) => {
+//   await connectDB();
+//   next();
+// });
+
+// // app.use(clientsRoutes);
+// // app.use(generateInvoiceRoutes);
+// // app.use(servicesRoutes);
+// // app.use(analyticsRoutes);
+
+// // ✅ test route (IMPORTANT)
+// app.get("/", (req, res) => {
+//   res.status(200).json({ success: true, message: "API working" });
+// });
+
+// export default app;
+
+
 import express from "express";
 import { configDotenv } from "dotenv";
 import connectDB from "./config/db.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-
-import authRoutes from "./routes/authRoutes.js";
-import clientsRoutes from "./routes/clientsRoutes.js";
-import generateInvoiceRoutes from "./routes/generateInvoiceRoutes.js";
-import servicesRoutes from "./routes/serviesRoutes.js";
-import analyticsRoutes from "./routes/analyticsRoutes.js";
 
 configDotenv();
 
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-  origin: true,
-  credentials: true,
-}));
+app.use("/auth", authRoutes);
 
-// 🔥 DB middleware (safe for serverless)
+// DB connect middleware
 app.use(async (req, res, next) => {
-  await connectDB();
-  next();
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    return res.status(500).json({ error: "DB FAILED" });
+  }
 });
 
-// app.use("/auth", authRoutes);
-// app.use(clientsRoutes);
-// app.use(generateInvoiceRoutes);
-// app.use(servicesRoutes);
-// app.use(analyticsRoutes);
-
-// ✅ test route (IMPORTANT)
 app.get("/", (req, res) => {
-  res.status(200).json({ success: true, message: "API working" });
+  res.json({ message: "DB OK" });
 });
 
 export default app;
