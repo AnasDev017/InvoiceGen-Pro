1b04bcc65cae495cba5eae0ccce6373212bfe1bf
AnasTahir84 <anastahirhussain7@gmail.com>
2026-02-02 01:05:24 +0500

    api test

--- BackEnd/middleware/Email.confiq.js
+++ BackEnd/middleware/Email.confiq.js
@@ -1,13 +1,16 @@
 import nodemailer from 'nodemailer'
+import { configDotenv } from 'dotenv'
 
-// Create a test account or replace with real credentials.
- export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
+configDotenv()
+
+// Create a transporter using environment variables
+export const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST || "smtp.gmail.com",
+  port: parseInt(process.env.SMTP_PORT) || 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: "anastahirhussain7@gmail.com",
-    pass: "nlrl kvxh wbib vphi",
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
